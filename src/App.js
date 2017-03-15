a65ce14c07966e359a34602a48cb50666afc2b4f
prbsas <paularamirez0316@gmail.com>
2017-03-15 12:30:19 -0400

    Use URL Parameters

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './App.css';
 
-const isActiveFn = (match, location) => {
+/*const isActiveFn = (match, location) => {
 	console.log(match, location)
 	return match
 }
@@ -30,4 +30,22 @@ const App = () => (
   </Router>
 );
 
+export default App;*/
+
+const App = () => (
+  <Router>
+    <div>
+      <Route path='/:page?-:subpage?' render={({match}) => (
+				<h1>
+					Page: {match.params.page || 'Home'}<br />
+					Subpage: {match.params.subpage}
+				</h1>
+			)} />
+
+    </div>
+  </Router>
+);
+
 export default App;
+
+// http://localhost:3000/react-router = page:react subpage:router
